d659eceb59d9b4a119d10b411273f893f2c3bda2
ErmekZhumagulov <erm.zhumagulov@gmail.com>
2023-07-13 17:21:25 +0600

    last of the day

--- main.py
+++ main.py
@@ -19,11 +19,11 @@ form.setupUi(window)
 window.show()
 errorWindow = QMessageBox()
 
-if __name__ == '__main__':
-    switching_tabs.switch_logic(form)
-    switching_btns.switch_logic(form)
-    labels_toggling.switch_logic(form)
-    # client = connection.setup_serial_connection(form)
+client = None
+timer = QTimer()
+
+def connect_to_plc():
+    global client
     try:
         client = Df1SerialClient(plc_type='SLC 5/04 CPU', src=0x1, dst=0x3,
                                  port='COM12',
@@ -31,20 +31,40 @@ if __name__ == '__main__':
                                  stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS,
                                  timeout=5)
         client.connect()
+        timer.start(1000)  # Start the timer for updating label_407
     except Exception as e:
-        client.reconnect()
-        print("reconnecting to plc -/-/-")
+        print("Error connecting to PLC:", str(e))
+        client = None
+        timer.stop()  # Stop the timer
+        reconnect_to_plc()  # Attempt to reconnect
 
-    def update_label_407():
-        form.label_407.setText(str(client.read_integer(start=1, total_int=1)[0]))
+def reconnect_to_plc():
+    global client
+    while client is None:  # Keep attempting to reconnect until successful
+        try:
+            connect_to_plc()
+        except KeyboardInterrupt:
+            app.quit()  # Exit the application if interrupted by the user
 
-    timer = QTimer()
-    timer.timeout.connect(update_label_407)
-    timer.start(1000)
+def update_label_407():
+    form.label_407.setText(str(client.read_integer(start=1, total_int=1)[0]))
 
-    def on_close():
+def on_close():
+    if client is not None:
         client.close()
+        timer.stop()  # Stop the timer
+    app.quit()
+
+if __name__ == '__main__':
+    window.showFullScreen()
     app.aboutToQuit.connect(on_close)
 
-window.showFullScreen()
-app.exec()
+    switching_tabs.switch_logic(form)
+    switching_btns.switch_logic(form)
+    labels_toggling.switch_logic(form)
+
+    # reconnect_to_plc()  # Start the initial connection process
+
+    timer.timeout.connect(update_label_407)  # Connect the timer to the update_label_407 function
+
+    app.exec()
